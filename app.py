e78aeb281a8e2a3517753fefaf18e20d444badf6
aeztor3210 <enrique_estor@yahoo.com>
2017-02-07 20:59:23 +0800

    added login functionality

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 
 import os
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, session, flash, request, redirect, url_for
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['USERNAME'] = 'admin'
+app.config['PASSWORD'] = 'secret'
+
+@app.route('/login', methods=['POST','GET'] )
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash("Welcome , {}".format(request.form['username']))
+            return redirect(url_for('homepage'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+     session.pop('logged_in', None)
+     flash('You were logged out')
+     return redirect(url_for('homepage'))
+
 
 @app.route('/')
-def Welcome():
-    return render_template('index.html')
+def homepage():
+    return render_template('homepage.html')
 
 @app.route('/myapp')
 def WelcomeToMyapp():
