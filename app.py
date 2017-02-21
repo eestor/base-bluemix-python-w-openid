980e1b66dd498e9305110990b999f28333d08bd6
aeztor3210 <enrique_estor@yahoo.com>
2017-02-22 00:21:26 +0800

    updated for openid authentication

--- app.py
+++ app.py
@@ -1,28 +1,70 @@
 
 import os
-from flask import Flask, render_template, jsonify, session, flash, request, redirect, url_for
+from flask import Flask, render_template, jsonify, session, flash, request, redirect, url_for, g
+from forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from flask_openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-app.config['USERNAME'] = 'admin'
-app.config['PASSWORD'] = 'secret'
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
-@app.route('/login', methods=['POST','GET'] )
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+import models
+
+@app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash("Welcome , {}".format(request.form['username']))
-            return redirect(url_for('homepage'))
-    return render_template('login.html', error=error)
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('homepage'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        session['logged_in'] = True
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = models.User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = models.User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(url_for('homepage'))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@lm.user_loader
+def load_user(id):
+    return models.User.query.get(int(id))
 
 @app.route('/logout')
 def logout():
+     logout_user()
      session.pop('logged_in', None)
      flash('You were logged out')
      return redirect(url_for('homepage'))
@@ -32,12 +74,9 @@ def logout():
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
 
-@app.route('/api/people')
-def GetPeople():
+@app.route('/api/users')
+def GetUsers():
     list = [
         {'name': 'John', 'age': 28},
         {'name': 'Bill', 'val': 26}
